70519e6048bf6c4e4f7ca27cc8ce2524108a3caa
MandyThuyanh.Nguyen <mandythuyanh.nguyen@student.htw-berlin.de>
2025-03-15 12:20:14 +0100

    Templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# dummy database
+posts = [
+    {
+        'author': 'Mandy',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 15, 2025'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 16, 2025'
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
+
