48a85be89de4f1f9c12260204b7ed7c73c3d572c
MandyThuyanh.Nguyen <mandythuyanh.nguyen@student.htw-berlin.de>
2025-03-15 14:52:28 +0100

    database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-# dummy database
-posts = [
-    {
-        'author': 'Mandy',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 15, 2025'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 16, 2025'
-    }
-]
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
